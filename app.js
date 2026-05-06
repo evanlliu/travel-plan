2b285060766266f9bd86e3c7737c520fef9c662f
Evan Liu <evan.liu@gmail.com>
2026-05-06 17:25:20 +0300

    1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  const APP_VERSION = "v2.42.0";
+  const APP_VERSION = "v2.42.1";
   const LS_DATA = "travel-plan-local-data";
   const LS_LANG = "travel-plan-ui-lang";
   const AUTO_REFRESH_MS = 60000;
